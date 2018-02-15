18d7067f6879f2a5cbc9dce10d57f709f0b22037
Quincy <qlam3@my.bcit.ca>
2018-02-15 14:34:51 -0800

    Updated to include BOATS

--- LmycDataLib/Models/IdentityModels.cs
+++ LmycDataLib/Models/IdentityModels.cs
@@ -10,6 +10,17 @@ namespace LmycWebSite.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string MobileNumber { get; set; }
+        public string SailingExperience { get; set; }
+
+        public string Role { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
